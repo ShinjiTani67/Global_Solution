8887d89399f7747811e6b53d43586774b19ff123
ShinjiTani67 <144123611+ShinjiTani67@users.noreply.github.com>
2023-11-27 10:05:53 -0300

    Add files via upload

---
+++ Global_Solution_CTP.py
@@ -0,0 +1,62 @@
+def menu():
+    print("opção de dicas de saúde (1)")
+    print("opção de calendario de vacinas(2)")
+    print("opção de nutrição(3)")
+    print("opção de calculo de IMC(4)")
+
+def login():
+    login = input("Você gostaria de fazer cadastro[c] ou um login[l]:")
+    while login != 0:
+        if login == "c".lower():
+            email = input("Digite o seu nome de usuário: ")
+            senha = input("Digite a senha: ")
+            print(f'O deu nome de usuário é {email}'' e a sua senha é ', f'{senha}' )
+        pass
+        elif login = "l".lower():
+            nome_usuario = input("Digite o seu nome de usuário: ")
+            senha = input("Digite a senha: ")
+            print(f'O deu nome de usuário é {nome_usuario}'' e a sua senha é ', f'{senha}' )
+            print("login efetuado")
+        pass
+        else:
+            print("opção não foi aceita")
+        pass
+
+def calculoIMC():
+    peso = float(input("Por favor digite o seu peso: "))
+    altura = float(input("Por favor digite a sua altura: "))
+    IMC = peso / (altura ** 2)
+    if IMC <= 18.5:
+        print("abaixo do peso ideal")
+    elif IMC >= 18.6 or IMC <= 24.9:
+        print("peso ideal")
+    elif IMC >= 25 or IMC <= 29.9:
+        print("Sobre peso")
+    elif IMC >= 30 or IMC <= 34.9:
+        print("Obesidade nivel 1")
+    elif IMC >= 35 or IMC <= 39.9:
+        print("Obesidade nivel 2")
+    else:
+        print("Obesidade nivel 3")
+
+#===============================================================================================================
+login()
+menu()
+opcao = int(input("Por favor digite a sua opção desejada: "))
+while opcao != 0:
+    if opcao == 1:
+        print("O menu de dicas abre opções de dicas sobre oque fazer em diversas situações como uma dor de cabeça por exemplo")
+        pass
+    elif opcao == 2:
+        print("O menu de caledário de vacinas deve dar a opção do usuário ver as vacinas que ele ja tomou")
+        pass
+    elif opcao == 3:
+        print("O menu de nutrição abre diverdsas dicas de como suplementar ")
+        pass
+    elif opcao == 4:
+        calculoIMC()
+        pass
+    else:
+        print("opcao não é valida")
+        pass
+    opcao = int(input("Por favor digite a sua opção desejada: "))
